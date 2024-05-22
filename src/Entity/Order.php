371df8d8d9cc41eb47c7026897a9717f2a6c762b
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2024-05-21 21:33:34 -0600

    Updating order entity to manage stickers

---
+++ src/Entity/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RandomSticker\SyliusStickerGeneratorPlugin\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_order")
+ */
+class Order
+{
+    /**
+     * @ORM\ManyToMany(targetEntity="RandomSticker\SyliusStickerGeneratorPlugin\Entity\Sticker")
+     * @ORM\JoinTable(name="order_stickers",
+     *      joinColumns={@ORM\JoinColumn(name="order_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="sticker_id", referencedColumnName="id")}
+     * )
+     */
+    private $stickers;
+
+    public function __construct()
+    {
+        $this->stickers = new ArrayCollection();
+    }
+
+    public function getStickers(): Collection
+    {
+        return $this->stickers;
+    }
+
+    public function addSticker(Sticker $sticker): self
+    {
+        if (!$this->stickers->contains($sticker)) {
+            $this->stickers[] = $sticker;
+        }
+        return $this;
+    }
+}
